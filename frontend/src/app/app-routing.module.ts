3097401ad05bdede57a1a169dbdc500eaeb751ba
s.pavlinina <sonya.pav25@gmail.com>
2023-04-03 23:06:54 +0300

    feat(front): Reload on the same URL navigation to run guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -31,7 +31,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload',
+    }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
